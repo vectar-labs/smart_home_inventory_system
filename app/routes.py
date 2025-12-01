fcff817873d6acd905001a06c178cb16ee2fcfb7
vectar-labs <abdulazizsadi31@gmail.com>
2025-12-01 22:19:36 +0300

    added dashboard page content

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,17 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
+# Authentication Routes
+
 @main.route('/')
 def login():
     return render_template('login.html')
 @main.route('/signup')
 def signup():
     return render_template('register.html')
+
+
+# Dashboard Routes  
+@main.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
