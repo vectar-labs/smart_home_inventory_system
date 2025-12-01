45812650d510c1d0f5526e1132a07b4806c37d34
vectar-labs <abdulazizsadi31@gmail.com>
2025-12-01 07:14:34 +0300

    added onboarding pages (login/register)

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,8 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def index():
-    return render_template('index.html')
+def login():
+    return render_template('login.html')
+@main.route('/signup')
+def signup():
+    return render_template('register.html')
