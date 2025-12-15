66c0229b2284b80d82828e8498c7dcb778475b02
vectar-labs <abdulazizsadi31@gmail.com>
2025-12-15 19:36:43 +0300

    modified users

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), nullable=False, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
