c2284e15d3c39b9c44b51d210543d5076996f3e4
vectar-labs <abdulazizsadi31@gmail.com>
2026-02-12 18:38:39 +0300

    Add Railway config

--- app/models.py
+++ app/models.py
@@ -3,8 +3,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
 
-
-
 class SuperUser(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), nullable=False, unique=True)
